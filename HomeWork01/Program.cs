6dfc93709f984745bf742ce7bee63146b25ac935
Alexandr Kopytov <kopytov.schura2014@yandex.ru>
2023-11-22 23:18:25 +0500

    Сделали Дз №1

--- HomeWork01/Program.cs
+++ HomeWork01/Program.cs
@@ -1,23 +1,27 @@
-﻿Console.Clear();
-Console.Write("Введите число: ");
-int firstNumber = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите число: ");
-int secondNumber = Convert.ToInt32(Console.ReadLine());
+﻿//Дз №1
+
+// Console.Clear();
+// Console.Write("Введите число: ");
+// int firstNumber = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число: ");
+// int secondNumber = Convert.ToInt32(Console.ReadLine());
  
- if(firstNumber > secondNumber)
-{
-Console.Write("Первое число " + firstNumber);
-Console.Write(" больше чем второе число " + secondNumber);
-}
-if(firstNumber < secondNumber)
-{
-Console.Write("Первое число " + firstNumber);
-Console.Write(" меньше чем второе число " + secondNumber);  
-}
-if(firstNumber == secondNumber)
-{
-Console.Write("Введенные числа равны " + firstNumber);
-}
+// if(firstNumber > secondNumber)
+// {
+// Console.Write($"Первое число `{firstNumber}`");
+// Console.Write($" больше чем второе число `{secondNumber}`");
+// }
+// if(firstNumber < secondNumber)
+// {
+// Console.Write($"Первое число `{firstNumber}`");
+// Console.Write($" меньше чем второе число `{secondNumber}`");  
+// }
+// if(firstNumber == secondNumber)
+// {
+// Console.Write($"Введенные числа равны `{firstNumber}`");
+// }
+
+
 
 
 
