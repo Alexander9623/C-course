b738f307d5dfb8869e140907ca64e3c204a066db
Alexandr Kopytov <kopytov.schura2014@yandex.ru>
2023-11-22 21:57:16 +0500

    Добавил HomeWork 01

---
+++ HomeWork01/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+ 
+ if(firstNumber > secondNumber)
+{
+Console.Write("Первое число " + firstNumber);
+Console.Write(" больше чем второе число " + secondNumber);
+}
+if(firstNumber < secondNumber)
+{
+Console.Write("Первое число " + firstNumber);
+Console.Write(" меньше чем второе число " + secondNumber);  
+}
+if(firstNumber == secondNumber)
+{
+Console.Write("Введенные числа равны " + firstNumber);
+}
+
+
+
+
+
+
+
+
+
+
+
+ 
