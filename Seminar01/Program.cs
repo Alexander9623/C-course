1343b153a163403a5605e8a27c9b0823c440f637
Alexandr Kopytov <kopytov.schura2014@yandex.ru>
2023-11-22 14:20:43 +0500

    добавил Seminar 01

---
+++ Seminar01/Program.cs
@@ -0,0 +1,23 @@
+﻿// comment
+
+/*
+comment
+comment
+*/
+Console.Clear();
+Console.Write("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());  
+Console.Write("Введите число");
+int b = Convert.ToInt32(Console.ReadLine());  
+
+if (a == b * b)
+    Console.WriteLine("Yes");
+else 
+    Console.WriteLine("No");
+
+
+
+
+
+
+
