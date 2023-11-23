6bfccc0c37ce2e4ad18e4f98c8aaa9770206f77b
Alexandr Kopytov <kopytov.schura2014@yandex.ru>
2023-11-23 14:04:03 +0500

    Сделали Дз №2

--- HomeWork01/Program.cs
+++ HomeWork01/Program.cs
@@ -22,11 +22,21 @@
 // }
 
 
+// Console.Clear();
+// Console.Write("Введите число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число: ");
+// int c = Convert.ToInt32(Console.ReadLine());
+ 
+// int max = a;
 
-
-
-
-
+// if(a > max) max = a;
+// if(b > max) max = b;
+// if(c > max) max = c;
+// Console.Write(max);
+// return max; Вместо консоли для автотестов
 
 
 
